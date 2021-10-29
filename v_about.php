c2176d1fbe571a7f6a10e831a77e833ced1ca301
bellafebriyanti <91323261+bellafebriyanti@users.noreply.github.com>
2021-10-29 15:18:04 +0700

    Add files via upload

---
+++ v_about.php
@@ -0,0 +1,24 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Bella</li>
+        <li>Nama Belakang : Febriyanti</li>
+    </ul>
+    <br>
+    <h4>Alamat</h4>
+    <ul type="none">
+        <li> Jalan Villa Indah Permai Blok D7 No 9 Jalan Poso 3 Bekasi Utara</li>
+    </ul>
+
+    <h4>Tempat Lahir</h4>
+    <ul type="none">
+        <li>Jakarta</li>
+    </ul>
+
+    <h4>Penyanyi Favorit</h4>
+    <ul type="square">
+        <li>Ruth B</li>
+        <li>Let her go</li>
+    </ul>
+</section>
